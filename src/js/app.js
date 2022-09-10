7c2f506d57966acbc6664721a39ae47375f5b85e
shahrizoda <shahrizodadeveloping@gmail.com>
2022-09-10 17:57:22 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,35 @@ functions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper();
+
+const menu = document.querySelector('.menu');
+const headerRight = document.querySelector('.header__right');
+const body = document.querySelector('body');
+
+menu.addEventListener('click', (e)=>{
+    menu.classList.toggle('active')
+    body.classList.toggle('hidden')
+    headerRight.classList.toggle('active') 
+})
+
+const headerIcon = document.querySelectorAll('.header__icon');
+const headerIconContent = document.querySelectorAll('.header__icon-content');
+const headerIconClose = document.querySelector('.header__icon-close');
+
+headerIcon.forEach(icon=>{
+    icon.addEventListener('click', (e)=>{
+        headerIconContent.forEach(contents=>{
+            contents.classList.remove('active')
+        })
+        const content = icon.querySelector('.header__icon-content')
+        content.classList.toggle('active')
+        headerIconClose.classList.toggle('active')
+    })
+})
+
+headerIconClose.addEventListener('click', (e)=>{
+    headerIconContent.forEach(contents=>{
+        contents.classList.remove('active')
+    })
+    headerIconClose.classList.remove('active')
+})
